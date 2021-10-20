d95fb19296493db608ed455cf98796ee994cef47
Yulia Nabieva <juliacoalnab@gmail.com>
2021-10-20 11:28:14 +0500

    HomeController modified

--- src/main/java/com/testProject/project/controller/HomeController.java
+++ src/main/java/com/testProject/project/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.testProject.project.controller;
 
+import com.testProject.project.entity.Assignment;
 import com.testProject.project.entity.Organization;
+import com.testProject.project.repository.AssignmentRepository;
 import com.testProject.project.repository.OrganizationRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,11 +12,20 @@ import java.util.Map;
 @Controller
 public class HomeController {
     OrganizationRepository organizationRepository;
+    AssignmentRepository assignmentRepository;
 
     @GetMapping("v1/home")
-    public String main(Map<String, Object> model) {
+    public String mainOrganizations(Map<String, Object> model) {
         Iterable<Organization> organizations = organizationRepository.findAll();
         model.put("organizations", organizations);
         return "home";
     }
+
+    @GetMapping("v1/home")
+    public String mainAssignments(Map<String, Object> model) {
+        Iterable<Assignment> assignments = assignmentRepository.findAll();
+        model.put("assignments", assignments);
+        return "home";
+    }
+
 }
